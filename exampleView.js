79766a770bba4aca949a56d03b116035d9d3f832
Hampuse1 <hampuse1@hotmail.com>
2016-03-23 16:20:55 +0100

    Update exampleView.js

--- exampleView.js
+++ exampleView.js
@@ -1,6 +1,5 @@
 //ExampleView Object constructor
 var ExampleView = function (container) {
-	
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
 	this.numberOfGuests = container.find("#numberOfGuests");
